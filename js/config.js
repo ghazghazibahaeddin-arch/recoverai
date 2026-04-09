6b2028c26a5683c49c1d9bc1e58fd1f343b0bb3e
ghazghazibahaeddin-arch <ghazghazibahaeddin@gmail.com>
2026-04-09 13:26:26 +0100

    Create config.js

---
+++ js/config.js
@@ -0,0 +1,11 @@
+const CONFIG = {
+  WORKER: 'https://clearcfo-proxy.ghazghazibahaeddin.workers.dev',
+  SUPABASE_URL: 'https://xwrnureztymlijhyfntn.supabase.co',
+  SUPABASE_KEY: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inh3cm51cmV6dHltbGlqaHlmbnRuIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzQ2MzI5MzUsImV4cCI6MjA5MDIwODkzNX0.PckXiWqz4-FPQoZkxqoEM16f3vivvE7qMUgZ9XZoqso',
+  GUMROAD: {
+    FREE: null,
+    STARTER: 'https://radhiayt.gumroad.com/l/fvrae',
+    PRO: 'https://radhiayt.gumroad.com/l/kcbbjx',
+    ENTERPRISE: 'https://radhiayt.gumroad.com/l/pevgar'
+  }
+};
