4a4c245bee86de1f8971bbad92d9e6d172e03627
ghazghazibahaeddin-arch <ghazghazibahaeddin@gmail.com>
2026-04-09 13:37:17 +0100

    Create app.js

---
+++ js/app.js
@@ -0,0 +1,9 @@
+document.addEventListener('DOMContentLoaded', function() {
+  NAV.init();
+  AUTH.init();
+  CHARGEBACK.init();
+  FRAUD.init();
+  LEAKAGE.init();
+  PLATFORM.init();
+  DASHBOARD.init();
+});
